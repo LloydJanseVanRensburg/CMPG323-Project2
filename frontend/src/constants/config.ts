1e61990be84bb67fb645d5b26973e75d3a9f0739
Lloyd Janse van Rensburg <thefullstackjunkie@gmail.com>
2021-11-22 07:41:39 +0200

    changed to api url

--- frontend/src/constants/config.ts
+++ frontend/src/constants/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-  apiURL: 'http://localhost:8000/api/v1',
-  // apiURL: 'http://172.104.244.191/api/v1',
+  // apiURL: 'http://localhost:8000/api/v1',
+  apiURL: 'http://172.104.244.191/api/v1',
 };
